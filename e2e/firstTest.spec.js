8967f36aebc589b5819d88d5659766f6aeedd478
sorawich-s63 <s6301012630176@email.kmutnb.ac.th>
2022-03-30 23:56:49 +0700

    set parent id

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -4,25 +4,25 @@ describe('Test-Todo', () => {
     setTimeout(() => { }, 2000);
   });
 
-  it('Add "Todo-List"', async () => {
-    await element(by.id('addbutton')).tap();
-    await element(by.id('title')).typeText("buy milk\n");
-    //await element(by.id('detail')).typeText("no milk\n");
-    //await expect(element(by.id('adddetail').withAncestor(by.id('detail')))).toBeVisible();
-    // await element(by.text('Note something?')).tap();
-    // await element(by.text('Note something?')).typeText("no milk\n");
-    await element(by.id('dateTimePicker')).tap();
-    await element(by.text('OK')).tap();
-    await expect(element(by.id('buy milk'))).toBeVisible();
-    await expect(element(by.id('savebutton'))).tap();
-    //await element(by.id('Addtodo').withAncestor(by.id('savebutton'))).tap()
-  });
+  // it('Add "Todo-List"', async () => {
+  //   await element(by.id('addbutton')).tap();
+  //   await element(by.id('title')).typeText("buy milk\n");
+  //   //await element(by.id('detail')).typeText("no milk\n");
+  //   //await expect(element(by.id('adddetail').withAncestor(by.id('detail')))).toBeVisible();
+  //   // await element(by.text('Note something?')).tap();
+  //   // await element(by.text('Note something?')).typeText("no milk\n");
+  //   await element(by.id('dateTimePicker')).tap();
+  //   await element(by.text('OK')).tap();
+  //   await expect(element(by.id('buy milk'))).toBeVisible();
+  //   await expect(element(by.id('savebutton'))).tap();
+  //   //await element(by.id('Addtodo').withAncestor(by.id('savebutton'))).tap()
+  // });
 
   it('Search Not Found "Todo-List"', async () => {
     await element(by.id('Search-text')).typeText("Go to the sea\n");
     await element(by.id('Search-button')).tap();
     await element(by.id('Search-button')).tap();
-    await waitFor( element(by.id('not-found')) ).toBeVisible().withTimeout(5000)
+    await waitFor(element(by.id('not-found'))).toBeVisible().withTimeout(5000)
   });
 
   it('Search Found "Todo-List"', async () => {
@@ -35,41 +35,32 @@ describe('Test-Todo', () => {
   });
 
   it('Status complete "Todo-List"', async () => {
-    //await element(by.id('status-button')).atIndex(1).tap();
     await element(by.id('status-button').withAncestor(by.id('Do Homework'))).tap();
-
-    // await expect(element(by.id("complete"))).toBeVisible();
-    // await expect( element(by.id('complete').withAncestor(by.id('Do Homework'))) ).toBeVisible();
-    // await expect( element(by.id('complete').withAncestor(by.id('Do Homework'))) ).toBeVisible();
-    //await expect( element(by.id('complete').withAncestor(by.id('Do Homework'))) ).toBeVisible();
-    await waitFor( element(by.id('complete').withAncestor(by.id('Do Homework'))) ).toBeVisible().withTimeout(5000)
-
+    await waitFor(element(by.id('complete').withAncestor(by.id('Do Homework')))).toBeVisible().withTimeout(5000)
   });
 
   it('Favourite "Todo-List"', async () => {
-
-    await element(by.id('fav-button').withAncestor(by.id('go shopping'))).tap();
-    await waitFor( element(by.id('favourite').withAncestor(by.id('go shopping'))) ).toBeVisible().withTimeout(5000)
-
+    await element(by.id('fav-button').withAncestor(by.id('Do Homework'))).tap();
+    await waitFor(element(by.id('favourite').withAncestor(by.id('Do Homework')))).toBeVisible().withTimeout(5000)
   });
 
   it('Edit "Todo-List"', async () => {
-      await waitFor( element(by.id('edit-button').withAncestor(by.id('go shopping'))) ).toBeVisible().withTimeout(2000);
-      await element(by.id('edit-button').withAncestor(by.id('go shopping'))).tap();
-      await element(by.id('edit-title')).replaceText("go to school\n");
-      await element(by.text('buy milk')).replaceText('pick the book');
-      await expect(element(by.text('go to school'))).toBeVisible();
-      await expect(element(by.text('pick the book'))).toBeVisible();
-      await element(by.id('edit-time')).tap();
-      await element(by.text('OK')).tap();
-      await element(by.id('update')).tap();
+    await waitFor(element(by.id('edit-button').withAncestor(by.id('Do Homework')))).toBeVisible().withTimeout(2000);
+    await element(by.id('edit-button').withAncestor(by.id('Do Homework'))).tap();
+    await element(by.id('edit-title')).replaceText("go to school\n");
+    await element(by.text('buy milk')).replaceText('pick the book');
+    await expect(element(by.text('go to school'))).toBeVisible();
+    await expect(element(by.text('pick the book'))).toBeVisible();
+    await element(by.id('edit-time')).tap();
+    await element(by.text('OK')).tap();
+    await element(by.id('update')).tap();
   });
 
   it('Delete "Todo-List"', async () => {
-      await waitFor( element(by.id('edit-button').withAncestor(by.id('go to school'))) ).toBeVisible().withTimeout(2000);
-      await element(by.id('edit-button').withAncestor(by.id('go to school'))).tap();
-      await element(by.id('delete')).tap();
-      await waitFor( element(by.id('edit-button').withAncestor(by.id('go to school'))) ).toBeNotVisible().withTimeout(2000);
+    await waitFor(element(by.id('edit-button').withAncestor(by.id('go to school')))).toBeVisible().withTimeout(2000);
+    await element(by.id('edit-button').withAncestor(by.id('Do Homework'))).tap();
+    await element(by.id('delete')).tap();
+    await waitFor(element(by.id('edit-button').withAncestor(by.id('go to school')))).toBeNotVisible().withTimeout(2000);
   });
-  
+
 });
